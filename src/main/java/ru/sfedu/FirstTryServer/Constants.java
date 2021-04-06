2926929778f27d69ca00d1bdb24c11cc8b771b91
Andrew <soan52005200@gmail.com>
2021-04-06 17:26:26 +0300

    06.04

--- src/main/java/ru/sfedu/FirstTryServer/Constants.java
+++ src/main/java/ru/sfedu/FirstTryServer/Constants.java
@@ -2,9 +2,8 @@ package ru.sfedu.FirstTryServer;
 
 public class Constants {
     public static String ENV_PROPERTIES="env";
-    public static String LOCAL_IP="192.168.2.";
+    public static String BACKUP1C_FOLDER="C:\\Users\\Андрей\\Desktop\\backup\\1c";
     public static final String LOG4J2_PROPERTIES = "log4j2.configurationFile";
     public static final String ENV_DEFAULT = "./src/main/resources/environment.properties";
-
     public static final String CSV_SERVER_KEY = "csvServer";
 }
--- src/main/java/ru/sfedu/FirstTryServer/Main.java
+++ src/main/java/ru/sfedu/FirstTryServer/Main.java
@@ -1,9 +1,25 @@
 package ru.sfedu.FirstTryServer;
 
 import java.io.IOException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import static ru.sfedu.FirstTryServer.Constants.ENV_PROPERTIES;
+import static ru.sfedu.FirstTryServer.Constants.LOG4J2_PROPERTIES;
 
 public class Main {
-    public static void main(String args[]) throws IOException{
+    private static final Logger log = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) throws IOException{
+        try {
+            log.debug(System.getProperty(ENV_PROPERTIES));
+            log.debug(System.getProperty(LOG4J2_PROPERTIES));
+            ;
+
+
+        } catch (Exception exception) {
+            log.error(exception);
+        }
 
     }
 }

3b66a9a6c443e21033bf89c64a3bd261efb0e9fd
soan52005200 <soan52005200@gmail.com>
2021-04-06 12:11:15 +0300

    Initial commit

---
+++ src/main/java/ru/sfedu/FirstTryServer/Main.java
@@ -0,0 +1,9 @@
+package ru.sfedu.FirstTryServer;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String args[]) throws IOException{
+
+    }
+}
---
+++ src/main/java/ru/sfedu/FirstTryServer/entity/Archieve.java
@@ -0,0 +1,5 @@
+package ru.sfedu.FirstTryServer.entity;
+
+public class Archieve {
+
+}

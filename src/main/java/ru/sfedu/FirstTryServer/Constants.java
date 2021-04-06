e50c27f37852fc31e4d49d9d07d092eec8d7ffb3
Andrew <soan52005200@gmail.com>
2021-04-06 17:02:16 +0300

    06.04

---
+++ src/main/java/ru/sfedu/FirstTryServer/Constants.java
@@ -0,0 +1,10 @@
+package ru.sfedu.FirstTryServer;
+
+public class Constants {
+    public static String ENV_PROPERTIES="env";
+    public static String LOCAL_IP="192.168.2.";
+    public static final String LOG4J2_PROPERTIES = "log4j2.configurationFile";
+    public static final String ENV_DEFAULT = "./src/main/resources/environment.properties";
+
+    public static final String CSV_SERVER_KEY = "csvServer";
+}

0f5f6ca1d3242f9eb0cc5a3180f266a67e12d43f
soan52005200 <soan52005200@gmail.com>
2021-04-07 03:16:54 +0300

    07.04

--- src/main/java/ru/sfedu/FirstTryServer/Constants.java
+++ src/main/java/ru/sfedu/FirstTryServer/Constants.java
@@ -3,7 +3,7 @@ package ru.sfedu.FirstTryServer;
 public class Constants {
     public static final Integer PORT=2228;
     public static String ENV_PROPERTIES="env";
-    public static String BACKUP1C_FOLDER="C:\\Users\\Андрей\\Desktop\\backup\\1c";
+    public static String BACKUP1C_FOLDER="backup1C";
     public static final String LOG4J2_PROPERTIES = "log4j2.configurationFile";
     public static final String ENV_DEFAULT = "./src/main/resources/environment.properties";
     public static final String CSV_SERVER_KEY = "csvServer";
--- src/main/java/ru/sfedu/FirstTryServer/api/ServerAnswer.java
+++ src/main/java/ru/sfedu/FirstTryServer/api/ServerAnswer.java
@@ -4,14 +4,21 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
+import static ru.sfedu.FirstTryServer.Constants.BACKUP1C_FOLDER;
 import static ru.sfedu.FirstTryServer.Constants.PORT;
+import static ru.sfedu.FirstTryServer.utils.ConfigurationUtil.getConfigurationEntry;
 
 public class ServerAnswer {
 
     public ServerAnswer() {
     try (var s = new ServerSocket(PORT))
+
     {
 
         try (Socket incoming = s.accept())
@@ -21,7 +28,12 @@ public class ServerAnswer {
 
             try (var in = new Scanner(inStream,StandardCharsets.UTF_8)){
                 var out = new PrintWriter(new OutputStreamWriter(outStream,StandardCharsets.UTF_8),true);
-                out.println("Hello, its nout ");
+                Path absolute = Paths.get(getConfigurationEntry(BACKUP1C_FOLDER));
+                try (Stream<Path> entries= Files.list(absolute)){
+
+                }
+
+
                 var done = false;
                 while (!done && in.hasNextLine()){
                     String line = in.nextLine();
